3adea75132aae6ba7cb9df2ac96705897d814f6c
Kyle <kyleyee522@gmail.com>
2024-03-20 14:00:51 -0700

    added express to test render

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('*', (req, res) => {
+	res.send(`A ${req.method} request to ${req.url}`);
+});
+
+app.listen(PORT, () => {
+	console.log('running on port ' + PORT);
+});
